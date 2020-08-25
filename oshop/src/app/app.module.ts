22638a5548b92058cd3ae2718a8c7a6f43f82e13
ithar.malik <ithar.malik@bjss.com>
2020-08-25 17:40:33 +0100

    Added shopping cart component

--- oshop/src/app/app.module.ts
+++ oshop/src/app/app.module.ts
@@ -12,13 +12,15 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ProductsComponent
+    ProductsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
