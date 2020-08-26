d8fc350fd48ffd5cb87ba0f4268d98db7b77af27
ithar.malik <ithar.malik@bjss.com>
2020-08-26 15:41:45 +0100

    Added sign with google

--- oshop/src/app/login/login.component.ts
+++ oshop/src/app/login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  private authProvider: any;
 
-  ngOnInit(): void {
+  constructor(private afAuth: AngularFireAuth) { 
+
   }
 
+  loginProvider(provider: number) {
+    
+    switch (provider) {
+      case 1: this.authProvider = new firebase.auth.GoogleAuthProvider();
+              break;
+      case 2: this.authProvider = new firebase.auth.FacebookAuthProvider();
+              break;
+      case 3: this.authProvider = new firebase.auth.TwitterAuthProvider();
+              break;
+      case 4: this.authProvider = new firebase.auth.GithubAuthProvider();
+    }
+
+    this.afAuth.signInWithRedirect(this.authProvider);
+  }
+  
 }
